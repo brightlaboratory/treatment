b1d46c93bb5057d850b7e9b3ace3e7814a3e5615
heemany <heemany.shekhar@gmail.com>
2018-04-17 20:15:44 -0700

    update1

--- treatment/src/main/scala/brightlaboratory/sjsu/edu/treatment/SimpleApp.scala
+++ treatment/src/main/scala/brightlaboratory/sjsu/edu/treatment/SimpleApp.scala
@@ -9,48 +9,94 @@ import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorAssembler}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.log4j.{Level, Logger}
+
 
 object PredictApp {
-  
-  def main(args:Array[String]){
-    
-    val conf = new SparkConf()
-    conf.set("spark.master", "local")
-    conf.set("spark.app.name", "exampleSpark")
-    val sc = new SparkContext(conf)
-    
-    val sparkSession = SparkSession.builder.
-      master("local")
-      .appName("spark session example")
-      .getOrCreate()
+  /*
+    def main(args:Array[String]){
 
-    SimpleApp.createDataframe(sparkSession)
-    
-  }
-    
+      val conf = new SparkConf()
+      conf.set("spark.master", "local")
+      conf.set("spark.app.name", "exampleSpark")
+      val sc = new SparkContext(conf)
+
+      val sparkSession = SparkSession.builder.
+        master("local")
+        .appName("spark session example")
+        .getOrCreate()
+
+      SimpleApp.createDataframe(sparkSession)
+
+    }
+      */
 }
 
 object SimpleApp {
 
   def createDataframe(spark: SparkSession) = {
-
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    Logger.getLogger("akka").setLevel(Level.ERROR)
     val df = spark.read
       .option("header", "true") //reading the headers
       .csv(getClass.getClassLoader.getResource("data.csv").getPath)
 
     // Convert all columns into integer
     val someCastedDF = (df.columns.toBuffer).foldLeft(df)((current, c) =>current.withColumn(c, col(c).cast("int")))
-    print(1)
 
     //Select 1) Age 2) Gender 3) Race 4) Ethnic 5) Marital Status 6) Education 7) Employment status 8) STFIPS
     val df_new = df.select("AGE", "GENDER","RACE","ETHNIC","MARSTAT","EDUC","STFIPS","SUB1")
-    someCastedDF.printSchema()
+    //someCastedDF.printSchema()
 
-    predictTreatmentCompletion(someCastedDF)
-//    multilayerPerceptronClassifier(df_new)
+    //preOrigDf.show(numRows = 1)
+    someCastedDF.createOrReplaceTempView("INPUT")
+
+    // get count of total success and failures
+    val total_success = spark.sql("select * from INPUT where REASON = 1").count()
+    val total_failure = spark.sql("select count(*) as TotalFailure, REASON from INPUT where REASON <> 1 group by REASON").agg(sum("TotalFailure" )).head()
+
+    //get success and failure for each SERVSETD categories
+    val SERVSETD_success = spark.sql("select SERVSETD, count(*) as count from INPUT WHERE REASON = 1 GROUP BY SERVSETD ORDER BY count DESC")
+    val SERVSETD_failure = spark.sql("select SERVSETD, count(*) as count from INPUT WHERE REASON <> 1 GROUP BY SERVSETD ORDER BY count DESC")
+
+    //for each SERVSETD category calculate it's success and failure probability
+    SERVSETD_success.createOrReplaceTempView("SERVSETD_success")
+    SERVSETD_failure.createOrReplaceTempView("SERVSETD_failure")
+    val success_prob = spark.sql(s"select SERVSETD, Round(count/${total_success}, 3) as s_prob from SERVSETD_success ")
+    val failure_prob = spark.sql(s"select SERVSETD, Round(count/${total_failure{0}}, 3) as f_prob from SERVSETD_failure ")
+    //success_prob.show()
+    //failure_prob.show()
+
+    //calculate rank of each SERVSETD
+    success_prob.createOrReplaceTempView("success_prob")
+    failure_prob.createOrReplaceTempView("failure_prob")
+    val SERVSETD_rankDf = spark.sql("select a.SERVSETD, Round(a.s_prob/b.f_prob , 3) " +
+      "as rank from success_prob a inner join failure_prob b ON a.SERVSETD = b.SERVSETD order by rank DESC")
+    SERVSETD_rankDf.show()
+    SERVSETD_rankDf.createOrReplaceTempView("SERV_RANK")
+
+    //Creating the new dataset where each failure record will be cross joined to produce all combinations of SERVSETD
+    var NEW_SERVSETD_FAILURE = spark.sql(
+      "select a.*, b.SERVSETD DERIVED_SERV, b.rank RANK " +
+        "from INPUT a cross join SERV_RANK b " +
+        "where a.REASON <> 1 " +
+        "order by CASEID")
+
+    NEW_SERVSETD_FAILURE.select("CASEID", "REASON","DISYR","SERVSETD","DERIVED_SERV").show(20)
+    //println(NEW_SERVSETD_FAILURE.count())
+
+    //Saving the dataset
+    NEW_SERVSETD_FAILURE.coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .option("header", "true")
+      .save("/home/heemany/Documents/treatment/TestData.csv")
+
+
+
+    //predictTreatmentCompletion(someCastedDF)
+    //multilayerPerceptronClassifier(df_new)
     //kMeansClustering(someCastedDF)
-   // calculateColumnValuePercentage(someCastedDF)
-
+    // calculateColumnValuePercentage(someCastedDF)
 
   }
 
@@ -79,22 +125,22 @@ object SimpleApp {
     val raceDF=assignOriginalColumnLabelsRACE(countRace)
 
 
-   // Calculating percentage of each unique value of AGE
-   def func_percentAge = udf((age:Int, count: Int, totalCount: Long) => {
-     val totalCountDouble = totalCount.toDouble
-     if (age == 2)      (count/totalCountDouble)*100
-     else if (age == 3) (count/totalCountDouble)*100
-     else if (age == 4) (count/totalCountDouble)*100
-     else if (age == 5) (count/totalCountDouble)*100
-     else if (age == 6) (count/totalCountDouble)*100
-     else if (age == 7) (count/totalCountDouble)*100
-     else if (age == 8) (count/totalCountDouble)*100
-     else if (age == 9) (count/totalCountDouble)*100
-     else if (age == 10) (count/totalCountDouble)*100
-     else if (age == 11) (count/totalCountDouble)*100
-     else if (age == 12) (count/totalCountDouble)*100
-     else 0
-   })
+    // Calculating percentage of each unique value of AGE
+    def func_percentAge = udf((age:Int, count: Int, totalCount: Long) => {
+      val totalCountDouble = totalCount.toDouble
+      if (age == 2)      (count/totalCountDouble)*100
+      else if (age == 3) (count/totalCountDouble)*100
+      else if (age == 4) (count/totalCountDouble)*100
+      else if (age == 5) (count/totalCountDouble)*100
+      else if (age == 6) (count/totalCountDouble)*100
+      else if (age == 7) (count/totalCountDouble)*100
+      else if (age == 8) (count/totalCountDouble)*100
+      else if (age == 9) (count/totalCountDouble)*100
+      else if (age == 10) (count/totalCountDouble)*100
+      else if (age == 11) (count/totalCountDouble)*100
+      else if (age == 12) (count/totalCountDouble)*100
+      else 0
+    })
 
 
     // val new_df1 = ageDF.withColumn("percentage", func_percentAge(col("AGE"),col("count")))
@@ -146,7 +192,7 @@ object SimpleApp {
   }
 
   def assignOriginalColumnLabelsRACE(dataFrame: DataFrame)= {
-   //Adding Actual Name Labels to column RACE
+    //Adding Actual Name Labels to column RACE
 
     val func_race: (Int => String) = (race: Int) => {
       if  (race == 1) "ALASKA NATIVE (ALEUT, ESKIMO, INDIAN)"
@@ -211,14 +257,14 @@ object SimpleApp {
     preOrigDf.createOrReplaceTempView("DATA")
     preOrigDf.sqlContext.sql("SELECT SERVSETD, LOS, REASON FROM DATA WHERE REASON = 1").show(10)
     preOrigDf.sqlContext.sql("SELECT SERVSETD, LOS, REASON FROM DATA WHERE REASON <> 1").show(10)
-//    System.exit(0)
+    //    System.exit(0)
 
     val column = "REASON"
 
     // If the treatment is completed, it is 1, everything else is set to 0
     val origDf = preOrigDf.withColumn(column, when(col(column).notEqual(1), 0).otherwise(1))
     origDf.show(10)
-    
+
     val changeDf = origDf.select(origDf("SERVSETD") + 1 as "cSERVSETD",origDf("METHUSE"), origDf("LOS")  as "cLOS", origDf("SUB1"),  origDf("ROUTE1"), origDf("NUMSUBS"), origDf("DSMCRIT"), origDf("REASON"))
     changeDf.show(10)
 
@@ -226,13 +272,13 @@ object SimpleApp {
     val labelIndexerModel = labelIndexer.fit(changeDf)
     val df = labelIndexerModel.transform(changeDf)
     df.show(10)
-//    val df = dfMod.withColumn("label", dfMod("REASON"))
-//    df.printSchema()
-//    df.show(5)
+    //    val df = dfMod.withColumn("label", dfMod("REASON"))
+    //    df.printSchema()
+    //    df.show(5)
 
     df.createOrReplaceTempView("average")
     df.sqlContext.sql("SELECT avg(cSERVSETD) as avgSERVSETD  FROM average GROUP BY REASON having REASON == 1 ").show
-    
+
     val assembler = new VectorAssembler().setInputCols(Array("cSERVSETD", "METHUSE", "LOS", "SUB1",
       "ROUTE1", "NUMSUBS", "DSMCRIT")).setOutputCol("features")
     val df2 = assembler.transform(df)
